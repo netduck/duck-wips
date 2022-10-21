6a35de003fe42545ca3bf076c7606e944bfcd2af
myharu01 <63537824+myharu01@users.noreply.github.com>
2022-10-21 20:40:33 +0900

    wips init

--- duck_wips.c
+++ duck_wips.c
@@ -10,7 +10,7 @@ void Eliminate(char *str, char* ch)
 {
     for (; *str != '\0'; str++)//종료 문자를 만날 때까지 반복
     {
-        if (*str == ch)//ch와 같은 문자일 때
+        if (*str == *ch)//ch와 같은 문자일 때
         {
             strcpy(str, str + 1);
             str--;
