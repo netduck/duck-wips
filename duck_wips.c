9a74eaffe4b1c0e39a0103c1a33775e6911db72b
myharu01 <63537824+myharu01@users.noreply.github.com>
2022-10-21 20:46:37 +0900

    wips init

--- duck_wips.c
+++ duck_wips.c
@@ -6,11 +6,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-void Eliminate(char *str, char* ch)
+void delete_char(char *str, char* ch)
 {
-    for (; *str != '\0'; str++)//종료 문자를 만날 때까지 반복
+    for (; *str != '\0'; str++)//NULL까지 반복
     {
-        if (*str == *ch)//ch와 같은 문자일 때
+        if (*str == *ch)//같은 글자라면
         {
             strcpy(str, str + 1);
             str--;
@@ -21,7 +21,7 @@ void Eliminate(char *str, char* ch)
 int main(int argc,char *argv[]){
     char *black_list[argc];
     for(int i=1;i<argc;i++){
-        Eliminate(argv[i],":");
+        delete_char(argv[i],":");
         black_list[i-1] = argv[i];
     }
     for(int i=0;i<argc-1;i++){
