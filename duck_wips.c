eb169a93c5e852b64eb84a843b0792c40292b24f
myharu01 <63537824+myharu01@users.noreply.github.com>
2022-10-21 21:34:50 +0900

    wips init

--- duck_wips.c
+++ duck_wips.c
@@ -39,6 +39,7 @@ int main(int argc,char *argv[]){
     unsigned char *Interface = argv[1];
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t *pcap = pcap_open_live(Interface, BUFSIZ, 1, 1000, errbuf);
+    printf("WTF!!!");
     if (pcap == NULL)
     {
         fprintf(stderr, "pcap_open_live(%s) return null - %s\n", param.dev_, errbuf);
