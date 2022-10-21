4d5584f6891c6b574fde83e7758c7b4df4c2a797
myharu01 <63537824+myharu01@users.noreply.github.com>
2022-10-21 21:29:25 +0900

    wips init

--- duck_wips.c
+++ duck_wips.c
@@ -31,8 +31,8 @@ int main(int argc,char *argv[]){
         printf("sudo ./duck_wips.c <interface> <mac_address1> <mac_address2> <mac_address3>...");
     }
     // char *black_list[argc];
-    char **black_list;
-    *black_list = (char **)malloc(sizeof(char*)*(argc-2));
+    char *black_list[17];
+    black_list = (char *)malloc(sizeof(char)*(argc-2));
     unsigned char *Interface = argv[1];
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t *pcap = pcap_open_live(Interface, BUFSIZ, 1, 1000, errbuf);
@@ -47,7 +47,7 @@ int main(int argc,char *argv[]){
         black_list[i-2] = *argv[i];
     }
     for(int i=0;i<argc-2;i++){
-        printf("%s",*black_list[i]);
+        printf("%s",black_list[i]);
     }
     
 }
